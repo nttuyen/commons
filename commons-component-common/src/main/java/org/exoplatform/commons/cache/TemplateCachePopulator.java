c29c97c300be3354c5403b00f036cb996b355c5f
Boubaker Khanfir <bkhanfir@exoplatform.com>
2016-10-09 19:14:20 +0100

    PLF-6814 Load groovy templates in parrallel as the server starts up
    Fix Resolver ResourceId. Without the servlet conext name, the templates with resolver != portal will not be cached.

--- commons-component-common/src/main/java/org/exoplatform/commons/cache/TemplateCachePopulator.java
+++ commons-component-common/src/main/java/org/exoplatform/commons/cache/TemplateCachePopulator.java
@@ -1,14 +1,17 @@
 package org.exoplatform.commons.cache;
 
+import java.security.PrivilegedAction;
 import java.util.List;
 
 import org.picocontainer.Startable;
 
+import org.exoplatform.commons.utils.SecurityHelper;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.container.xml.ValuesParam;
 import org.exoplatform.groovyscript.text.TemplateService;
 import org.exoplatform.resolver.ApplicationResourceResolver;
+import org.exoplatform.resolver.ResourceKey;
 import org.exoplatform.resolver.ServletResourceResolver;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
@@ -18,7 +21,6 @@ public class TemplateCachePopulator implements Startable {
 
   TemplateService             templateService;
 
-  ApplicationResourceResolver resolver;
 
   List<String>                templates;
 
@@ -27,12 +29,7 @@ public class TemplateCachePopulator implements Startable {
   public TemplateCachePopulator(TemplateService templateService, PortalContainer container, InitParams initParams) {
     this.templateService = templateService;
     this.container = container;
-    resolver = new ApplicationResourceResolver();
-    resolver.addResourceResolver(new ServletResourceResolver(container.getPortalContext(), "war:"));
-    resolver.addResourceResolver(new ServletResourceResolver(container.getPortalContext(), "app:"));
-    resolver.addResourceResolver(new ServletResourceResolver(container.getPortalContext(), "system:"));
-    resolver.addResourceResolver(new ServletResourceResolver(container.getPortalContext().getContext("/eXoResources"),
-                                                             "resources:"));
+
     ValuesParam valuesParam = initParams.getValuesParam("templates");
     if (valuesParam != null) {
       templates = valuesParam.getValues();
@@ -56,12 +53,32 @@ public class TemplateCachePopulator implements Startable {
     new Thread(new Runnable() {
       @Override
       public void run() {
-        Thread.currentThread().setContextClassLoader(container.getPortalClassLoader());
+        SecurityHelper.doPrivilegedAction(new PrivilegedAction<ClassLoader>() {
+          public ClassLoader run() {
+            Thread.currentThread().setContextClassLoader(container.getPortalClassLoader());
+            return Thread.currentThread().getContextClassLoader();
+          }
+        });
+
+        CachePopulatorResourceResolver resolver = new CachePopulatorResourceResolver();
+
+        resolver.addResourceResolver(new ServletResourceResolver(container.getPortalContext(), "war:"));
+        resolver.addResourceResolver(new ServletResourceResolver(container.getPortalContext(), "app:"));
+        resolver.addResourceResolver(new ServletResourceResolver(container.getPortalContext(), "system:"));
+        resolver.addResourceResolver(new ServletResourceResolver(container.getPortalContext().getContext("/eXoResources"),
+                                                                 "resources:"));
         LOG.info("Start caching templates");
         long started = System.currentTimeMillis();
         for (String templateId : partOfTemplates) {
           try {
             LOG.debug("Cache template {}", templateId);
+            if(templateId.contains("@")) {
+              String[] templateParts = templateId.split("@");
+              templateId = templateParts[1];
+              resolver.setContextPath(templateParts[0]);
+            } else {
+              resolver.setContextPath(null);
+            }
             templateService.getTemplate(templateId, resolver, true);
           } catch (Exception e) {
             LOG.debug("Error cache template {}, cause = {}", templateId, e.getMessage());
@@ -80,4 +97,21 @@ public class TemplateCachePopulator implements Startable {
   public void stop() {
   }
 
+  public static class CachePopulatorResourceResolver extends ApplicationResourceResolver {
+
+    private String contextPath;
+
+    public CachePopulatorResourceResolver() {
+    }
+
+    @Override
+    public ResourceKey createResourceKey(String url) {
+      
+      return new ResourceKey(contextPath.hashCode(), url);
+    }
+    
+    public void setContextPath(String contextPath) {
+      this.contextPath = contextPath;
+    }
+  }
 }
